12ea7ca23502b0bb8c59619d8e681f8e67efec00
AllenWangyc <451366033@qq.com>
2025-01-17 10:28:04 +1300

    Implement filter feature

--- frontend-dashboard/src/apis/index.jsx
+++ frontend-dashboard/src/apis/index.jsx
@@ -1,43 +1,5 @@
 /* global chrome */
-
-import { apiFetch } from '@/apis/apiConfig.jsx'
 import { request } from '@/utils'
-
-const API_DOMAIN = 'http://localhost:5050/'
-
-export const API_CODE = {
-  // API success, data success
-  OK: 200,
-  // API success, data exception
-  ERR_DATA: 403,
-  // API success, data empty
-  ERR_NO_DATA: 301,
-  // API success, login exception
-  ERR_LOGOUT: 401
-}
-
-// Report content of API request error
-export const API_FAILED = 'There is something wrong with net connection, please try again later.'
-
-// API integration
-export const apiReqs = {
-  // Login with Google account
-  signInByGoggle: (config) => {
-    return new Promise((resolve, reject) => {
-      config.url = API_DOMAIN + 'auth/google/'
-      config.method = 'get'
-      apiFetch(config)
-    })
-  },
-  // Login with Google account
-  signInByGithub: (config) => {
-    return new Promise((resolve, reject) => {
-      config.url = API_DOMAIN + 'auth/github/'
-      config.method = 'get'
-      apiFetch(config)
-    })
-  },
-}
 
 export function historyFetchAPI() {
   return request({
